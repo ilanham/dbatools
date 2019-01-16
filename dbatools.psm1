6fd167bbb48b9d1d0bf8cc421aa8a0b2df18715c
Chrissy LeMaire <clemaire@gmail.com>
2019-01-16 17:09:28 +0100

    try with dll

--- dbatools.psm1
+++ dbatools.psm1
@@ -206,7 +206,7 @@ if ($script:multiFileImport) {
 
 } else {
     Add-Type -Assembly System.IO.Compression.FileSystem
-    $zip = [System.IO.Compression.ZipFile]::OpenRead((Resolve-Path -Path "$script:PSModuleRoot\allcommands.zip"))
+    $zip = [System.IO.Compression.ZipFile]::OpenRead((Resolve-Path -Path "$script:PSModuleRoot\allcommands.dll"))
     $stream = $zip.Entries.Open()
     $reader = New-Object IO.StreamReader($stream)
     $ExecutionContext.InvokeCommand.InvokeScript($false, ([scriptblock]::Create(($reader.ReadToEnd()))), $null, $null)
